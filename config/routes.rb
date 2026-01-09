121bcc6f86a48c7eac1cf37ca60efca3a7a68b7d
ae0f893 <kawaguchi.ryohei.nv@gmail.coml>
2026-01-09 15:28:20 +0900

    feat: 7章

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static_pages#home"
+
+  resources :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+    User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobarbaz", password_confirmation: "foobarbaz")
